a63a9e3bc39dd3d6347a4735b7af2349d34f1852
Quant1um <31078520+Quant1um@users.noreply.github.com>
2019-06-02 18:48:25 +0300

    Update virtualc

--- 88_virtualc.lua
+++ 88_virtualc.lua
@@ -1,55 +1,97 @@
 local component = component or require("component")
 local computer = computer or require("computer")
- 
+
 local pairs = pairs
 local getmetatable = getmetatable
 local type = type
- 
-local function getmetaprop(table, prop)
- local metatable = getmetatable(table)
- if type(metatable) == "table" then return rawget(metatable, prop) end
- return nil
-end
- 
-local function isNaN(val)
- return type(val) == "number" and val ~= val
-end
- 
-local virtualComponents = {}
- 
-local function getType(virtual, address)
- local componentType = getmetaprop(virtual, "__type")
- if type(componentType) == "function" then
-  return componentType(virtual, address)
- elseif componentType == nil then
-  return "virtual"
- else
-  return tostring(componentType)
+local rawget = rawget
+
+local function checkType(val, ...)
+ local valType = type(val)
+ for _, t in pairs({...}) do
+  if valType == t then
+   return true
+  end
  end
+ return false
 end
- 
+
+local virtualComponents = {}
+local virtualProxies = {}
+
 local function isVirtual(address)
- assert(address ~= nil and not isNaN(address), "address shouldn't be nil or nan")
+ checkArg(1, address, "string")
  return not not virtualComponents[address]
 end
+
+local defaultProxyMetatable = {
+ __index = function(self, key)
+  if key == "slot" then return self:getSlot() end -- dynamic slot
+  return rawget(self, key)
+ end
+}
+
+local defaultGetMethodInfo = function() return true, "undocumented" end 
+local defaultGetFields = function() return {} end
+local defaultGetSlot = function() return -1 end
+local defaultCreateProxy = function(self)
+ local proxy = setmetatable({}, defaultProxyMetatable)
  
-local function mountComponent(address, proxy)
- assert(address ~= nil and not isNaN(address), "address shouldn't be nil or nan")
- assert(type(proxy) == "table", "proxy should be a table")
+ local address = self.address
+
+ proxy.type = self.type
+ proxy.address = address
+ 
+ for _, method in pairs(self:getMethods(address)) do
+  proxy[method] = function(...) 
+   return self:invokeMethod(method, ...) 
+  end
+ end
+
+ return proxy
+end
+
+local function mountComponent(address, descriptor)
+ checkArg(1, address, "string")
+ checkArg(2, descriptor, "table")
+
+ assert(checkType(descriptor.type, "string"),                   "invalid descriptor: type must be a string")
+ assert(checkType(descriptor.createProxy, "function", "nil"),   "invalid descriptor: createProxy must be a function or nil")
+ assert(checkType(descriptor.invokeMethod, "function"),         "invalid descriptor: invokeMethod must be a function")
+ assert(checkType(descriptor.getMethodInfo, "function", "nil"), "invalid descriptor: getMethodInfo must be a function or nil")
+ assert(checkType(descriptor.getMethods, "function"),           "invalid descriptor: getMethods must be a function")
+ assert(checkType(descriptor.getFields, "function", "nil"),     "invalid descriptor: getFields must be a function or nil")
+ assert(checkType(descriptor.getSlot, "function", "nil"),       "invalid descriptor: getSlot must be a function or nil")
+
  assert(not component.proxy(address), "component with given address already attached")
- 
- virtualComponents[address] = proxy
- computer.pushSignal("component_added", address, getType(proxy, address))
+
+ descriptor.address = address
+ descriptor.getMethodInfo = descriptor.getMethodInfo or defaultGetMethodInfo
+ descriptor.getFields = descriptor.getFields or defaultGetFields
+ descriptor.getSlot = descriptor.getSlot or defaultGetSlot
+ descriptor.createProxy = descriptor.createProxy or defaultCreateProxy
+
+ virtualComponents[address] = descriptor
+ computer.pushSignal("component_added", address, descriptor.type)
 end
- 
+
 local function unmountComponent(address)
- assert(address ~= nil and not isNaN(address), "address shouldn't be nil or nan")
+ checkArg(1, address, "string")
  assert(isVirtual(address), "address should point to virtual component")
- 
+
+ local descType = virtualComponents[address].type
  virtualComponents[address] = nil
- computer.pushSignal("component_removed", address, getType(proxy, address))
+ virtualProxies[address] = nil
+ computer.pushSignal("component_removed", address, descType)
 end
- 
+
+local function getDescriptor(address)
+ checkArg(1, address, "string")
+ assert(isVirtual(address), "address should point to virtual component")
+
+ return virtualComponents[address]
+end
+
 local oldComponent
 do -- intercept the component library
  local oldDoc = component.doc
@@ -71,138 +113,95 @@ do -- intercept the component library
   slot = oldSlot,
   type = oldType
  }
- 
+
  function component.doc(address, method)
   local virtual = virtualComponents[address]
   if virtual then
-   local metaDoc = getmetaprop(virtual, "__doc")
-   local metaDocType = type(metaDoc)
-   if metaDocType == "function" then
-    return metaDoc(virtual, address, method)
-   elseif metaDocType == "table" then
-    local doc = metaDoc[method]
-    if doc then return doc end
-    return nil
-   else
-    local method = virtual[method]
-    if method then return tostring(method) end
-    return nil
-   end
+   local direct, doc = virtual:getMethodInfo(method)
+   return doc
   end
- 
+
   return oldDoc(address, method)
  end
- 
+
  function component.list(filter, exact)
   local l = oldList(filter, exact)
- 
+  
   for k, v in pairs(virtualComponents) do
-   local type = getType(v, k)
+   local type = v.type
    if not filter or (exact and filter == type) or (not exact and filter:find(type:sub(1, #filter))) then
     l[k] = type
    end
   end
- 
+  
   return l
  end
- 
+
  function component.proxy(address)
   local virtual = virtualComponents[address]
   if virtual then
-   local metaProxy = getmetaprop(virtual, "__proxy")
-   local metaProxyType = type(metaProxy)
-   
-   if metaProxyType == "table" then return metaProxy end
-   if metaProxyType == "function" then return metaProxy(virtual, address) end
-   return virtual
+   local proxy = virtualProxies[address]
+   if not proxy then
+    proxy = virtual:createProxy()
+    virtualProxies[address] = proxy
+   end
+   return proxy
   end
- 
+
   return oldProxy(address)
  end
  
  function component.invoke(address, method, ...)
   local virtual = virtualComponents[address]
   if virtual then
-   local metaInvoke = getmetaprop(virtual, "__invoke")
-   
-   if type(metaInvoke) == "function" then return invokeMethod(virtual, address, method, ...) end
-   
-   local vmethod = virtual[method]
-   if vmethod then return vmethod(...) end
-   error("no such method")  
+   return virtual:invokeMethod(method, ...)  
   end
- 
+
   return oldInvoke(address, method, ...)
  end
  
  function component.methods(address)
   local virtual = virtualComponents[address]
   if virtual then
-   local metaMethods = getmetaprop(virtual, "__methods")
-   local metaMethodsType = type(metaMethods)
-   
-   if metaMethodsType == "function" then return metaMethods(virtual, address) end
-   if metaMethodsType == "table" then return metaMethods end
-   
    local methods = {}
-   for k, v in pairs(virtual) do
-    if type(v) == "function" then
-     methods[k] = true
-    end
+   for _, method in pairs(virtual:getMethods()) do
+    local direct, doc = virtual:getMethodInfo(method)
+    methods[method] = direct
    end
    return methods
   end
- 
+
   return oldMethods(address)
  end
  
  function component.fields(address)
   local virtual = virtualComponents[address]
   if virtual then
-   local metaFields = getmetaprop(virtual, "__fields")
-   local metaFieldsType = type(metaFields)
-   
-   if metaFieldsType == "function" then return metaFields(virtual, address) end
-   if metaFieldsType == "table" then return metaFields end
-   
-   local fields = {}
-   for k, v in pairs(virtual) do
-    if type(v) ~= "function" then
-     fields[k] = true
-    end
-   end
-   return fields
+   return virtual:getFields()
   end
- 
+
   return oldFields(address)
  end
  
  function component.slot(address)
   local virtual = virtualComponents[address]
   if virtual then
-   local metaSlot = getmetaprop(virtual, "__slot")
-   if type(metaSlot) == "function" then
-    return metaSlot(virtual, address)
-   elseif metaSlot == nil then
-    return -1
-   else
-    return metaSlot
-   end  
+   return virtual:getSlot()
   end
- 
+
   return oldSlot(address)
  end
  
  function component.type(address)
   local virtual = virtualComponents[address]
   if virtual then
-   return getType(virtual, address)
+   return virtual.type
   end
- 
+
   return oldType(address)
  end
 end
- 
+
 package.loaded["virtualc"] = {
  isVirtual = isVirtual,
  mount = mountComponent,
